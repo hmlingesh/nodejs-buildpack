4e593de5782bab9914874e52d2b933adeb8857e6
lingeshm <lingesh.m@covisint.com>
2018-06-21 18:31:50 +0530

    oracle nodedb client libs

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -26,37 +26,6 @@ detect_memory() {
   esac
 }
 
-install_oracle_libraries() {
-  echo "Installing oracle libraries"
-  local ORACLE_DIR=/home/vcap/app/.cloudfoundry/oracle
-  mkdir -p $ORACLE_DIR
-  cd $ORACLE_DIR
-  basic_download_url="https://s3.amazonaws.com/covisintrnd.com-software/instantclient-basic.zip"
-  sdk_download_url="https://s3.amazonaws.com/covisintrnd.com-software/instantclient-sdk.zip"
-  curl -LOk "$basic_download_url"
-  echo "Downloaded [$basic_download_url]"
-  curl -LOk "$sdk_download_url"
-  echo "Downloaded [$sdk_download_url]"
-  echo $PWD
-  echo "unzipping libraries"
-   unzip instantclient-basic.zip
-   unzip instantclient-sdk.zip
-  mv instantclient_12_2 instantclient
-  cd instantclient
-  echo $PWD
-  ln -s libclntsh.so.12.1 libclntsh.so
-  echo $PWD
-}
-
-install_oracle_libraries
-
-echo "----setting oracle env vars----"
-export LD_LIBRARY_PATH=/home/vcap/app/.cloudfoundry/oracle/instantclient:${LD_LIBRARY_PATH:-}
-export OCI_LIB_DIR=/home/vcap/app/.cloudfoundry/oracle/instantclient
-export OCI_INC_DIR=/home/vcap/app/.cloudfoundry/oracle/instantclient/sdk/include
-echo "ld library path = ${LD_LIBRARY_PATH:-}"
-echo "----/setting oracle env vars----"
-
 calculate_concurrency
 
 export MEMORY_AVAILABLE=$MEMORY_AVAILABLE
